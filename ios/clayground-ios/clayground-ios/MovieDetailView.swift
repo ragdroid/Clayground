0e3a9fba7f8ed52edff6c1ad12df9cf87354f5c2
Ritesh Gupta <rg.riteshh@gmail.com>
2021-03-21 00:50:28 +0530

    update to new kmp

--- ios/clayground-ios/clayground-ios/MovieDetailView.swift
+++ ios/clayground-ios/clayground-ios/MovieDetailView.swift
@@ -9,51 +9,65 @@ import SwiftUI
 import shared
 
 final class MovieDetailViewState: ObservableObject {
-	let collector = Collector()
-    lazy var viewModel = NativeViewModel<MovieDetailState, MovieDetailEvent, MovieDetailViewEffect>(viewModel: clayground_iosApp.appComponent!.resolve(MovieDetailViewModel.self)!, nativeCallback: collector)
+	@Published var name: String = "Initial movie name"
+	@Published var isLoading: Bool = true
+
+	lazy var viewModel = NativeViewModel<MovieDetailState, MovieDetailEvent, MovieDetailViewEffect>(viewModel: kmpViewModel, render: handleRender, viewEffectHandler: handleViewEffect)
+
+	private lazy var kmpViewModel = clayground_iosApp.appComponent!.resolve(MovieDetailViewModel.self)!
+
+	private func handleRender(_ state: MovieDetailState?) {
+		isLoading = state?.loadingState == LoadingState.Loading()
+		name = state?.movieDetails?.title ?? "A movie has no name"
+	}
+
+	private func handleViewEffect(_ effect: MovieDetailViewEffect?) {
+	}
 
 	func handleOnAppear() {
         viewModel.dispatchEvent(event: MovieDetailEvent.Load())
 	}
 
 	func handleOnDisappear() {
-//		viewModel.onDestroy()
+		viewModel.onDestroy()
 	}
-}
-
-extension MovieDetailViewState {
-	final class Collector: NativeCallback<MovieDetailState, MovieDetailEvent, MovieDetailViewEffect> {
-		override func handleViewEffects(viewEffect: MovieDetailViewEffect?) {
-            PlatformKt.kermitLogger().d(message: "Inside view effects: \(viewEffect!)", tag: "MovieDetailViewState", throwable: nil)
-		}
-		override func render(state: MovieDetailState?) {
-            PlatformKt.kermitLogger().d(message: "Inside render: \(state!)", tag: "MovieDetailViewState", throwable: nil)
-
-		}
-	}
-    final class NewCollector: Kotlinx_coroutines_coreFlowCollector {
-        func emit(value: Any?, completionHandler: @escaping (KotlinUnit?, Error?) -> Void) {
-            print("Inside NewCollecttor: \(value)")
-        }
-        
-        
-    }
-    
 }
 
 struct MovieDetailView: View {
 	@ObservedObject var state: MovieDetailViewState
 
 	var body: some View {
-		Text(Greeting().greeting())
-			.padding()
+		contentView()
 			.onAppear(perform: state.handleOnAppear)
 			.onDisappear(perform: state.handleOnDisappear)
 	}
 }
 
+private extension MovieDetailView {
+	func contentView() -> some View {
+		if state.isLoading {
+			return loadingView().eraseToAnyView()
+		} else {
+			return nameView().eraseToAnyView()
+		}
+	}
+	func nameView() -> some View {
+		Text(state.name)
+			.padding()
+	}
+	func loadingView() -> some View {
+		LoaderView()
+	}
+ }
+
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		MovieDetailView(state: .init())
+	}
+}
+
+extension View {
+	func eraseToAnyView() -> AnyView {
+		AnyView(self)
 	}
 }
