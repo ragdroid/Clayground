9c41fd753773a2c7c9a5f7e2c8f1caeffe8e875b
Ritesh Gupta <rg.riteshh@gmail.com>
2021-03-21 17:24:03 +0530

    Add mac app 😎

---
+++ clayground-mac/clayground-mac/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  clayground-mac
+//
+//  Created by Ritesh Gupta on 21/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
